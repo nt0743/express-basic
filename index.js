b957580ab00bb4fc712ab51fc1fad2478f129f41
tn09874309862 <137918369+nt0743@users.noreply.github.com>
2023-06-27 16:22:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-  response.send("It worked!")
+  response.send("It worked again!")
 })
 
 app.listen(3000)
