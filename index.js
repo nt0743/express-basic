8f432c203daa43d5f3f4cdf5e357624ae8e37da6
tn09874309862 <137918369+nt0743@users.noreply.github.com>
2023-06-28 20:01:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ app.get("/", (request, response) => {
   console.log("Logging a get request");
 });
 
-app.get("/routeparameters/:routeParamName", (request, response) => {
+app.get(
+  "/routeparameters/:routeParamName",
+  identifyRouteParameter(request, response)
+);
+/*app.get("/routeparameters/:routeParamName", (request, response) => {
   response.send(`Your route parameter is: ${request.params.routeParamName}`);
-});
+});*/
 
-// /queryparameters?queryParamName=value
-app.get("/queryparameters", (request, response) => {
+// URL of form /queryparameters?queryParamName=value
+app.get("/queryparameters", identifyQueryParameter(request, response));
+/*app.get("/queryparameters", (request, response) => {
   response.send(
     `The value of query parameter 'queryParamName' is: ${request.query.queryParamName}`
   );
-});
+});*/
 
 app.listen(3000);
+
+function identifyRouteParameter(request, response) {
+  response.send(`Your route parameter is: ${request.params.routeParamName}`);
+}
+
+function identifyQueryParameter(request, response) {
+  response.send(
+    `The value of query parameter 'queryParamName' is: ${request.query.queryParamName}`
+  );
+}
